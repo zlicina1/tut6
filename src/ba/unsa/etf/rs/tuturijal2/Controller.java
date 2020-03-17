364f8c0e75bd2aec4b4090508c33c530888b6bf5
zlicina1 <zlicina1@etf.unsa.ba>
2020-03-17 16:17:54 +0100

    Povezan Controller sa sample.fxml

--- src/ba/unsa/etf/rs/tuturijal2/Controller.java
+++ src/ba/unsa/etf/rs/tuturijal2/Controller.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rs.tuturijal2;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label prezime_labela;
+
+    public void onClick(ActionEvent actionEvent) {
+        System.out.println("Button otvoren");
+        prezime_labela.getText();
+    }
 }
